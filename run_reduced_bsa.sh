38c5a3e6e665ccdf21ae35e0cd225a4bc5ff9137
KennethLangedal <kenneth.langedal@hotmail.com>
2026-02-10 11:29:12 +0100

    reduced + bsa

---
+++ run_reduced_bsa.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_graph_file>"
+    exit 1
+fi
+
+INPUT_FILE=$1
+BASENAME=$(basename "$INPUT_FILE" .graph)
+JSON_FILE="${BASENAME}.json"
+OUTPUT_DIR="${BASENAME}"
+
+# If the input file path is relative, make it absolute.
+if [[ "$INPUT_FILE" != /* ]]; then
+    INPUT_FILE="$(pwd)/$INPUT_FILE"
+fi
+
+# Make sure we are in the script's directory, so we can find the executables.
+cd "$(dirname "$0")"
+
+# echo "Processing $INPUT_FILE..."
+
+LR_OUT=$(./MWIS_REDUCE "$INPUT_FILE" --time_limit=1800 --kernel="$BASENAME")
+
+LR_TIME=$(echo "$LR_OUT" | awk -F',' '{print $11}')
+LR_N=$(echo "$LR_OUT" | awk -F',' '{print $8}')
+
+REMAINING_TIME=$(echo "3600 - $LR_TIME" | bc)
+
+if [ "$LR_N" -eq 0 ]; then
+    echo "$LR_OUT"
+else
+
+    # echo "1. Converting to JSON..."
+    METIS_OUT=$(./METIS_TO_JSON "$BASENAME".kernel_graph "$JSON_FILE")
+
+    # echo "2. Creating output directory..."
+    mkdir -p "$OUTPUT_DIR"
+
+    # echo "3. Running BSA_GREEDY..."
+    BSA_OUT=$(timeout -s SIGTERM "$REMAINING_TIME" ./BSA_GREEDY "$JSON_FILE" 16)
+
+    METIS_TIME=$(echo "$METIS_OUT" | awk -F',' '{print $3}')
+    BSA_TIME=$(echo "$BSA_OUT" | awk -F',' '{print $1}')
+    CHILS_TIMEOUT=$(echo "3600 - $LR_TIME - $METIS_TIME - $BSA_TIME" | bc)
+
+    # echo "4. Running CHILS for $CHILS_TIMEOUT seconds..."
+    CHILS_OUT=$(./MWIS_CHILS -g "$BASENAME".kernel_graph -f "$OUTPUT_DIR" -t "$CHILS_TIMEOUT" -c 1)
+
+    echo "$LR_OUT,$METIS_OUT,$BSA_OUT,$CHILS_OUT"
+
+    rm "$BASENAME"_weight.csv
+    rm "$BASENAME".csv
+    rm "$BASENAME".kernel_graph
+
+    # echo "5. Cleaning up..."
+    rm -rf "$JSON_FILE"
+    rm -rf "$OUTPUT_DIR"
+fi
